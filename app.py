1b224a45ddfbe16a2b9a629f08a2cf57c5730603
Adithya K Shetty <adithyakshetty01@gmail.com>
2023-04-29 23:21:04 +0530

    New Update

--- app.py
+++ app.py
@@ -15,12 +15,6 @@ def hello_world():
    filename = werkzeug.utils.secure_filename(imagefile.filename)
    print("\nReceived image File name : " + imagefile.filename)
    imagefile.save("./runs/images/"+filename)
-   return redirect(url_for('detect'))
-   # return redirect(url_for('inputImage'))
-
-
-@app.route("/detect")
-def detect():
    output_result = ""
    img = cv2.imread('./runs/images/androidFlask.jpg')
    cv2.imwrite('./runs/images/output.jpeg', img)
@@ -36,6 +30,25 @@ def detect():
         print(model.names[int(c)])
    print("------------RESULT ENDS HERE---------------")
    return output_result
+
+
+# @app.route("/detect")
+# def detect():
+#    output_result = ""
+#    img = cv2.imread('./runs/images/androidFlask.jpg')
+#    cv2.imwrite('./runs/images/output.jpeg', img)
+
+#    image = Image.open('./runs/images/output.jpeg')
+#    detections = model.predict(image,conf=0.25,hide_labels=True,hide_conf=True,save=True)
+#    print("------------RESULT STARTS FROM HERE---------------")
+#    for r in detections:
+#       for c in r.boxes.cls:
+        
+#         output_result += model.names[int(c)].lower()
+#         output_result += "@"
+#         print(model.names[int(c)])
+#    print("------------RESULT ENDS HERE---------------")
+#    return output_result
 
 # @app.route('/inputImage',methods = ['GET', 'POST'])
 # def inputImage():
