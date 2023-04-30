6d92a6980d362a1e56d560479712709723baa5e5
Adithya K Shetty <adithyakshetty01@gmail.com>
2023-04-30 08:35:48 +0530

    Signify Backend v.1.2.1

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ def hello_world():
    imagefile = request.files['image']
    filename = werkzeug.utils.secure_filename(imagefile.filename)
    print("\nReceived image File name : " + imagefile.filename)
-   imagefile.save("./runs/images/"+filename)
+   # imagefile.save("./runs/images/"+filename)
    output_result = ""
-   img = cv2.imread('./runs/images/androidFlask.jpg')
-   cv2.imwrite('./runs/images/output.jpeg', img)
+   # img = cv2.imread('./runs/images/androidFlask.jpg')
+   # cv2.imwrite('./runs/images/output.jpeg', img)
 
-   image = Image.open('./runs/images/output.jpeg')
-   detections = model.predict(image,conf=0.25,hide_labels=True,hide_conf=True,save=True)
+   # image = Image.open('./runs/images/output.jpeg')
+   detections = model.predict(imagefile,conf=0.25,hide_labels=True,hide_conf=True)
    print("------------RESULT STARTS FROM HERE---------------")
    for r in detections:
       for c in r.boxes.cls:
